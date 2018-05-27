4c38f2ca3f76ed5b4648b4842d105a35bbe0c6d9
alayor <alayor3@gmail.com>
2018-05-27 15:13:36 -0600

    all offers should have estomated price

--- features/customer/offers.feature
+++ features/customer/offers.feature
@@ -3,3 +3,4 @@ Feature: Product Offers
 
 Scenario: Get Product Offers Information
   When I send request to get product offers
+  Then all offers should have estimated price
--- support/steps/customer/offers.js
+++ support/steps/customer/offers.js
@@ -1,9 +1,16 @@
 const { Given, When, Then } = require('cucumber')
 const OffersRequest = require('support/requests/customer-api/offers')
 const { expect } = require('chai')
+const R = require('ramda')
 
 When('I send request to get product offers', async function () {
   const request = new OffersRequest.Builder()
     .build()
   await this.send(request)
+})
+
+Then('all offers should have estimated price', function () {
+  const offersWithNoEstimatedPrice = R.filter(o => !o.estimated_price, this.lastResponse.data)
+
+  expect(offersWithNoEstimatedPrice).to.be.empty
 })
