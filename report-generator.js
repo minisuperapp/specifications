e4db6a4b71a99074c26ddb2fac7c143ea939c7cd
alayor <alayor3@gmail.com>
2018-05-26 20:27:36 -0600

    Ad step to save artifact

--- report-generator.js
+++ report-generator.js
@@ -3,7 +3,7 @@ var reporter = require('cucumber-html-reporter');
 var options = {
     theme: 'bootstrap',
     jsonFile: 'results/cucumber_report.json',
-    output: `${process.env.CIRCLE_ARTIFACTS || 'results'}/cucumber_report.html`,
+    output: 'results/cucumber_report.html',
     reportSuiteAsScenarios: true,
     launchReport: false,
     metadata: {}
