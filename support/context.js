41a6dc1bacda0eb8c7c74c90eca7ba755ab4ef67
alayor <alayor3@gmail.com>
2018-05-28 12:31:02 -0600

    Attach requests and responses to json result

--- support/context.js
+++ support/context.js
@@ -2,14 +2,18 @@ const { setWorldConstructor } = require('cucumber')
 const apiRequester = require('support/apiRequester')
 
 class Context {
-  constructor() {
+  constructor(attach) {
+    this.attach = attach
     this.lastResponse = {}
     this.currentRequest = {}
   }
 
   async send(request) {
+    this.attach.attach(`${request.method} /${request.path}`)
+    this.attach.attach(JSON.stringify(request.body), 'application/json')
     this.lastResponse = await apiRequester.send(request, this.sessionToken)
-    
+    this.attach.attach(JSON.stringify(this.lastResponse), 'application/json')
+
     return this.lastResponse
   }
 
