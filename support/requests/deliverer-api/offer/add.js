21d358254d4165e1730d77e34c86f6ba16556024
alayor <alayor3@gmail.com>
2018-06-01 14:18:43 -0600

    Add addOfferRequest

---
+++ support/requests/deliverer-api/offer/add.js
@@ -0,0 +1,36 @@
+class AddOfferRequest {
+  constructor(build) {
+
+  }
+  get method() {
+    return 'POST'
+  }
+  get path() {
+    return 'offer/add'
+  }
+  get body() {
+    return {
+        "productId": "1",
+        "availableQuantity": "8",
+        "onHandQuantity": "10",
+        "unitPrice": "18",
+      	"delivererLocation": {
+      		"latitude": "27.670799",
+      		"longitude": "105.1599679,16"
+      	}
+    }
+  }
+  static get Builder() {
+    class Builder {
+         constructor() {
+
+         }
+         build() {
+            return new AddOfferRequest(this)
+         }
+      }
+      return Builder
+  }
+}
+
+module.exports = AddOfferRequest
