ab8d21b25e4f3246290c9237e2062d9031d3121c
alayor <alayor3@gmail.com>
2018-06-02 12:47:47 -0600

    Fix validations

--- support/steps/customer/offers.js
+++ support/steps/customer/offers.js
@@ -3,10 +3,10 @@ const OffersRequest = require("support/requests/customer-api/offers")
 const { expect } = require("chai")
 const R = require("ramda")
 
-When("I send request to get offers for product {int}", async function(productId) {
-  const request = new OffersRequest.Builder()
-    .withProductId()
-    .build()
+When("I send request to get offers for product {int}", async function(
+  productId
+) {
+  const request = new OffersRequest.Builder().withProductId().build()
   await this.send(request)
 })
 
@@ -14,25 +14,32 @@ Then("I should receive one offer", function() {
   expect(this.lastResponse.data.length).to.equal(1)
 })
 
-Then("all offers should have an id, price, and estimated arrival time", function() {
-  const offersWithMissingDetails = R.compose(
-    R.filter(o => !o.id),
-    R.filter(o => !o.price),
-    R.filter(o => !o.estimated_arrival_time),
-  )(this.lastResponse.data)
+Then(
+  "all offers should have an id, price, and estimated arrival time",
+  function() {
+    const offersWithMissingDetails = R.filter(
+      o => !o.id || !o.price || !o.estimated_arrival_time,
+      this.lastResponse.data
+    )
 
-  expect(offersWithMissingDetails).to.be.empty
-})
+    expect(offersWithMissingDetails).to.be.empty
+  }
+)
 
-Then("all offers should have the deliverer name, reputation, and last rating", function() {
-  const deliverersWithMissingDetails = R.compose(
-    R.filter(o => !o.deliverer.name),
-    R.filter(o => !o.deliverer.reputation),
-    R.filter(o => !o.deliverer.last_rating),
-  )(this.lastResponse.data)
+Then(
+  "all offers should have the deliverer name, reputation, and last rating",
+  function() {
+    const deliverersWithMissingDetails = R.filter(
+      o =>
+        !o.deliverer.name ||
+        !o.deliverer.reputation ||
+        !o.deliverer.last_rating,
+      this.lastResponse.data
+    )
 
-  expect(deliverersWithMissingDetails).to.be.empty
-})
+    expect(deliverersWithMissingDetails).to.be.empty
+  }
+)
 
 Then("offers should be ordered by estimated arrival time", function() {
   const arrivalTimes = R.map(
