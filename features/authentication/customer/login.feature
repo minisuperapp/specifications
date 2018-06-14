b339b3d223fcff9a7b8694195767b417badbcebd
alayor <alayor3@gmail.com>
2018-06-14 07:39:17 -0600

    Make sure customer login works well

--- features/authentication/customer/login.feature
+++ features/authentication/customer/login.feature
@@ -4,5 +4,5 @@ Background:
   Given Customer registers with phone number '6483516383'
 
 Scenario: Successful login
-  When Customer logs in with phone number '6483516383' and password 'secret1'
+  When Customer logs in with phone number '6483516383'
   Then Customer should receive successful response
--- support/steps/customer/auth.js
+++ support/steps/customer/auth.js
@@ -1,14 +1,18 @@
 const { Given, When, Then } = require('cucumber')
 const CustomerRegistrationRequest = require('support/requests/customer-api/registration')
+const CustomerLoginRequest = require('support/requests/customer-api/login')
 const { expect } = require('chai')
 
-When('Customer registers with phone number {string}', async function (phoneNumber) {
+When('Customer registers with phone number {string}', async function(phoneNumber) {
   const request = new CustomerRegistrationRequest.Builder()
     .withPhoneNumber(phoneNumber)
     .build()
   await this.send(request)
 })
 
-When('Customer logs in with phone number {string} and password {string}', function (string, string2) {
-
+When('Customer logs in with phone number {string}', async function(phoneNumber) {
+  const request = new CustomerLoginRequest.Builder()
+    .withPhoneNumber(phoneNumber)
+    .build()
+  await this.send(request)
 })
