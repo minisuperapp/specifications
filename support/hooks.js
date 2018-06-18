40c0b26b2a6754fdd7235a58079f4851c12f4848
alayor <alayor3@gmail.com>
2018-06-17 20:10:00 -0600

    Clean customer api in hooks

--- support/hooks.js
+++ support/hooks.js
@@ -2,6 +2,8 @@ const config = require('../config')
 const redis = require('thunk-redis')
 const { Before, AfterAll } = require('cucumber')
 const Bluebird = require('bluebird')
+const CustomerApiCleanRequest = require('./requests/customer-api/clean')
+const apiRequester = require('support/api_requester')
 const redisClient = redis.createClient(config.redis_host, {
   usePromise: Bluebird,
   returnBuffers: false,
@@ -11,9 +13,11 @@ const redisClient = redis.createClient(config.redis_host, {
 })
 
 Before(async function (testCase) {
+  await apiRequester.send(new CustomerApiCleanRequest.Builder().build())
   return await redisClient.flushall()
 })
 
 AfterAll(async function () {
+  await apiRequester.send(new CustomerApiCleanRequest.Builder().build())
   return await redisClient.quit()
 })
