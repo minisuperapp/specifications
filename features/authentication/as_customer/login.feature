228cac42074aa89a269ac8389953f0ce854987b0
alayor <alayor3@gmail.com>
2018-06-20 20:08:53 -0600

    Add scenario to make sure customer receives a session token

--- features/authentication/as_customer/login.feature
+++ features/authentication/as_customer/login.feature
@@ -6,6 +6,7 @@ Background:
 Scenario: Successful login
   When Customer logs in with phone number '6483516383'
   Then Customer should receive successful response
+  And Customer should receive session token
 
 Scenario: Wrong phone number
   When Customer logs in with phone number '6394516222'
--- support/steps/customer/auth.js
+++ support/steps/customer/auth.js
@@ -16,3 +16,8 @@ When('Customer logs in with phone number {string}', async function(phoneNumber) 
     .build()
   await this.send(request)
 })
+
+Then('Customer should receive session token', function() {
+  expect(this.lastResponse.data.sessionToken).not.to.be.undefined
+  expect(this.lastResponse.data.sessionToken.length).to.be.at.least(1)
+})
