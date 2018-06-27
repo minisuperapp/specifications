cc690bd671c039c24ab1a8cc4b0f862be930655e
alayor <alayor3@gmail.com>
2018-06-26 23:07:58 -0600

    Enable hooks to clean deliverers table

--- support/api_requester.js
+++ support/api_requester.js
@@ -8,7 +8,7 @@ api_requester.send = async (request, sessionToken) => {
     body: request.method === 'GET' ? undefined : JSON.stringify(request.body),
     headers: {
       'Content-Type': 'application/json',
-      'session-Token': sessionToken,
+      'Session-Token': sessionToken,
       'is-test': 'true',
     },
   }
