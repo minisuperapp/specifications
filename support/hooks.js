d439d65a0cbfba54cd2eceaa22980985d4ea2cac
alayor <alayor3@gmail.com>
2018-06-30 17:55:00 -0600

    Add socket.io support

--- support/hooks.js
+++ support/hooks.js
@@ -5,6 +5,8 @@ const { Before, AfterAll } = require('cucumber')
 const Bluebird = require('bluebird')
 const DelivererApiCleanRequest = require('./requests/deliverer-api/clean')
 const apiRequester = require('support/api_requester')
+const io = require('socket.io-client')
+const socket = io(`${process.env.CUSTOMER_API_URL || 'http://localhost:3000'}`)
 const Knex = require('knex')
 const KnexFile = require('./knexfile')
 const knex = Knex(KnexFile)
@@ -17,11 +19,16 @@ const redisClient = redis.createClient(config.redis_host, {
 })
 
 Before(async function(testCase) {
+  socket.emit('socketping')
+  socket.on('socketpong', () => {
+    console.log('received, sending...')
+  })
   await knex('deliverers').del()
   return await redisClient.flushall()
 })
 
 AfterAll(async function() {
+  socket.disconnect()
   await knex('deliverers').del()
   await knex.destroy()
   return await redisClient.quit()
