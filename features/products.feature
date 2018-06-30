3c2082a722aa944861fac730d29135a8fb583dc1
alayor <alayor3@gmail.com>
2018-06-29 21:46:31 -0600

    Add scenario to make sure we get available status for products

--- features/products.feature
+++ features/products.feature
@@ -5,3 +5,5 @@ Scenario: Get Products
   Then all products should have an id
   And all products should have a name
   And all products should have a code
+  And all products should have available status
+  And all products should be unavailable
---
+++ support/steps/products.js
@@ -0,0 +1,35 @@
+const { Given, When, Then } = require('cucumber')
+const ProductsRequest = require('support/requests/customer-api/products')
+const { expect } = require('chai')
+const R = require('ramda')
+
+When('I send request to get products', async function () {
+  const request = new ProductsRequest.Builder()
+    .build()
+  await this.send(request)
+})
+
+Then('all products should have an id', function () {
+  const productsWithNoId = R.filter(o => !o.id, this.lastResponse.data)
+  expect(productsWithNoId).to.be.empty
+})
+
+Then('all products should have a name', function () {
+  const productsWithNoName = R.filter(o => !o.name, this.lastResponse.data)
+  expect(productsWithNoName).to.be.empty
+})
+
+Then('all products should have a code', function () {
+  const productsWithNoCode = R.filter(o => !o.code, this.lastResponse.data)
+  expect(productsWithNoCode).to.be.empty
+})
+
+Then('all products should have available status', function () {
+  const productsWithNoAvailableStatus = R.filter(o => o.available === undefined, this.lastResponse.data)
+  expect(productsWithNoAvailableStatus).to.be.empty
+})
+
+Then('all products should be unavailable', function () {
+  const availableProducts = R.filter(o => o.available === true, this.lastResponse.data)
+  expect(availableProducts).to.be.empty
+})
