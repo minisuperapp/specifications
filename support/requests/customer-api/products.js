f617ec076071201dc82ce64b175be1319876e0fb
alayor <alayor3@gmail.com>
2018-06-30 16:07:14 -0600

    Send customer location to get products

--- support/requests/customer-api/products.js
+++ support/requests/customer-api/products.js
@@ -3,6 +3,8 @@ const Base = require('./$base')
 class ProductsRequest extends Base {
   constructor(build) {
     super()
+    this.customerLocationLatitude = build.customerLocationLatitude
+    this.customerLocationLongitude = build.customerLocationLongitude
   }
   get method() {
     return 'POST'
@@ -11,18 +13,24 @@ class ProductsRequest extends Base {
     return 'products'
   }
   get body() {
-    return {}
+    return {
+      customerLocation: {
+        latitude: this.customerLocationLatitude,
+        longitude: this.customerLocationLongitude,
+      },
+    }
   }
   static get Builder() {
     class Builder {
-         constructor() {
-
-         }
-         build() {
-            return new ProductsRequest(this)
-         }
+      constructor() {
+        this.customerLocationLatitude = '28.1867048'
+        this.customerLocationLongitude = '-105.4600849'
       }
-      return Builder
+      build() {
+        return new ProductsRequest(this)
+      }
+    }
+    return Builder
   }
 }
 
