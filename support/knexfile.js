b550e668847db14cda73045078757f3bb38109ff
Alonso A. Ortega <alayor3@gmail.com>
2018-11-09 20:30:54 -0700

    Fix db credentials.

--- support/knexfile.js
+++ support/knexfile.js
@@ -3,9 +3,9 @@ module.exports = {
   searchPath: 'public',
   connection: {
     database: 'test',
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'root',
+    port: process.env.DB_PORT || 15432,
   },
 }
