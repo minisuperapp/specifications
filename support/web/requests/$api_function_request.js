78fc5787ff323c98d4278fbbd658353623b70e77
alayor <alayor3@gmail.com>
2022-03-24 23:00:38 -0700

    Remove deliverer not null restruction on request.

--- support/web/requests/$api_function_request.js
+++ support/web/requests/$api_function_request.js
@@ -2,9 +2,6 @@ const config = require('config')
 
 class ApiFunctionRequest {
   constructor(deliverer) {
-    if (!deliverer) {
-      throw new Error('deliverer should not be null!')
-    }
     this.uri = `${config.api_functions_host}`
     this.apiServer = 'api-functions'
     this.deliverer = deliverer
