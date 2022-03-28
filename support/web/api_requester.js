3e354b65d1f163a7de6615da6141e63ab6504e6f
alayor <alayor3@gmail.com>
2022-03-27 17:39:24 -0700

    Get cookies as usual no matter the come from api-functions.

--- support/web/api_requester.js
+++ support/web/api_requester.js
@@ -1,6 +1,5 @@
 require('isomorphic-fetch')
 const cookie = require('cookie')
-const ApiFunctionRequest = require('../web/requests/$api_function_request')
 
 const api_requester = {}
 
@@ -25,14 +24,9 @@ api_requester.send = async (request, customer_session_token, deliverer_session_t
       }
     }
     responseText = await res.text()
-    const customerSessionToken = getSessionTokenCookie(res, responseText, 'customer_session_token')
-    const delivererSessionToken = getSessionTokenCookie(
-      res,
-      responseText,
-      'deliverer_session_token',
-    )
-    const parsedResponse = JSON.parse(responseText)
-    const body = request instanceof ApiFunctionRequest ? parsedResponse.body : parsedResponse
+    const customerSessionToken = getSessionTokenCookie(res, 'customer_session_token')
+    const delivererSessionToken = getSessionTokenCookie(res, 'deliverer_session_token')
+    const body = JSON.parse(responseText)
 
     return Promise.resolve({
       ...body,
@@ -50,18 +44,11 @@ api_requester.send = async (request, customer_session_token, deliverer_session_t
   }
 }
 
-function getSessionTokenCookie(res, responseText, cookie_name) {
+function getSessionTokenCookie(res, cookie_name) {
   if (res.headers && res.headers._headers && res.headers._headers['set-cookie']) {
     const foundCookie = res.headers._headers['set-cookie'].find(c => c.startsWith(cookie_name))
     if (foundCookie) {
       return cookie.parse(foundCookie)[cookie_name]
-    }
-  }
-  const response = JSON.parse(responseText)
-  if (response.headers && response.headers['set-cookie']) {
-    const foundCookie = response.headers['set-cookie'].includes(cookie_name)
-    if (foundCookie) {
-      return cookie.parse(response.headers['set-cookie'])[cookie_name]
     }
   }
 }
